801f88fa4fa640d3c60bd667580cda82ee4c3042
evezmemmedli <avazkm@code.edu.az>
2022-06-01 09:11:54 +0400

    Add project files.

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewResult view = new ViewResult
+            {
+                ViewName = "HomePage"
+            };
+            return view;
+        }
+
+        public ViewResult About()
+        {
+            return new ViewResult { ViewName = "about" };
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+
+        }
+        public ActionResult Contact()
+        {
+            return View();
+
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController:Controller
+    {
+        public ActionResult Detail(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+    }
+}
